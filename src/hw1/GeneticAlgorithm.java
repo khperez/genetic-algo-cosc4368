90afaf67d248f7239e042309a018c38c7e28205a
Denny Luong <denny.luong@outlook.com>
2017-01-30 10:23:59 -0600

    test

--- src/hw1/GeneticAlgorithm.java
+++ src/hw1/GeneticAlgorithm.java
@@ -5,7 +5,7 @@ public class GeneticAlgorithm {
 	public int genMaker() 
 	   {
 	        int genChromosome;
-	        return genChromosome;
+	        return genChromosome
 	   }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
