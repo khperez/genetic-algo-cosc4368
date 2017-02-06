ee06b9372f6cad2f75ed4208c0de72ff3d526637
Denny Luong <denny.luong@outlook.com>
2017-02-06 11:33:21 -0600

    Test merge population plot

--- src/hw1/GeneticAlgorithm.java
+++ src/hw1/GeneticAlgorithm.java
@@ -1,202 +1,126 @@
 package hw1;
 
-import java.util.Random;
-public class GeneticAlgorithm {
+import org.jfree.ui.RefineryUtilities;
 
-	public static int[] randChromosomes() 
-    {
-		Random randChromo = new Random(System.currentTimeMillis());
-        int genChromosome[] = new int[20];
-        int j = 0;
-        while(j < 20)
-        {
-        	genChromosome[j] = randChromo.nextInt()%682 + 1;
-        	if((genChromosome[j] > 0) && (genChromosome[j] < 682))
-        	{
-        		j++;
-        	}
-        }
-        return genChromosome;
-    }
-	
-	public static int[] crossover(int[] population, double pco) {
-		int crossoverTotal = (int) (pco * population.length);
-		int crossoverMask = 0b1111100000;
-		Random mateTime = new Random(System.currentTimeMillis());
-		for (int i = 0; i < crossoverTotal; i = i+2) {
-			int chromosomeA, chromosomeB, chromosomeY, chromosomeZ = 0;
-			chromosomeA = population[i] & crossoverMask;
-			chromosomeB = population[i+1] & ~crossoverMask;
-			chromosomeY = population[i] & ~crossoverMask;
-			chromosomeZ = population [i+1] & crossoverMask;
+public class GeneticAlgorithm {
 			
-			int chromoOne = mateTime.nextInt()%20;
-			while(chromoOne < 0)
-			{
-				chromoOne = mateTime.nextInt()%20;
-			}
-			population[chromoOne] = chromosomeA | chromosomeB;
-			int chromoTwo = mateTime.nextInt()%20;
-			while((chromoTwo < 0) || (chromoOne == chromoTwo))
-			{
-				chromoTwo = mateTime.nextInt()%20;	
-			}
-			population[chromoTwo] = chromosomeY | chromosomeZ;	
-		}
-		
-		return population;
-	}
-	
-	public static int calculateFitness(int chromosome) {
-		int fitness = 0;
-		
-		for (int count = 0; count < 10; count++) {
-			if (count % 2 == 0) { 
-				if ((chromosome & 1) == 0)
-					fitness++;
-			}
-			else
-				if ((chromosome & 1) == 1)
-					fitness++;
-			chromosome = chromosome >> 1;
-		}
-		
-		return fitness;
-	}
-	
-	public static int[] mutatorGenerator(int[] population)
-	{
-		Random select = new Random(System.currentTimeMillis());
-		int mutator = select.nextInt()%20;
-		while(mutator < 0)
-		{
-			mutator = select.nextInt()%20;
-		}
-//		System.out.println("Mutator index: " + mutator);
-		int chromosome = population[mutator];
-		int mutatedBit = select.nextInt()%10;
-		while(mutatedBit < 0)
-		{
-			mutatedBit = select.nextInt()%10;
-		}
-//		System.out.println("Mutated bit: " + mutatedBit);
-		int mask = 1;
-		int mutatedChromosome = chromosome ^ (mask << mutatedBit);
-		population[mutator] = mutatedChromosome;
-		return population;
-	}
-	
 	public static void main(String[] args) {
-		double pco = 0;
-		int[] nPco = new int[5];
-		String[] sPco = new String[5];
-		String mutStr = "";
-		String crossStr = "";
-		int[] population = randChromosomes();
-		for(int ii = 0 ; ii < 5 ; ii++)
-		{
-			switch(ii)
-			{
-				case 0:System.out.println("case: " + ii);
-						pco = 0.7;
-				break;
-				case 1:System.out.println("case: " + ii);
-						pco = 0.3;
-				break;
-				case 2:System.out.println("case: " + ii);
-						pco = 0.5;
-				break;
-				case 3:System.out.println("case: " + ii);
-						pco = 0.9;
-				break;
-				case 4:System.out.println("case: " + ii);
-						pco = 0;
-				break;
-				default:System.out.println("Error");
-				break;
-			}
 		
-			int[] nextGeneration = new int[20], mutatedGeneration = new int[20];
-//			double pco = 0.7;
-			boolean maxFitnessAchieved = false;
-			int generationCount = 1;
-			
-			boolean fitnessMutation = false, fitnessCrossover = false;
-			
-//			System.out.println("Initial Population");
-//			System.out.println("Chromosome\tFitness value:");	
-			for(int i = 0; i < population.length; i++) {
-				System.out.println( String.format("%10s\t", 
-									Integer.toBinaryString(population[i])).replace(' ', '0') 
-									+ calculateFitness(population[i]));
-				// Check fitness value
-				if (calculateFitness(population[i]) == 10) {
-					break;
-				}
-			}
-			
-			
-			for (generationCount = 2 ; !maxFitnessAchieved; generationCount++) {
-				
-				nextGeneration = crossover(population, pco);
-				
-//				System.out.println("Generation " + generationCount);
-				for(int i = 0; i < nextGeneration.length; i++) {
-					System.out.println( String.format("%10s\t", 
-										Integer.toBinaryString(nextGeneration[i])).replace(' ', '0') 
-										+ calculateFitness(nextGeneration[i]));
-					// Check fitness value
-					if (calculateFitness(nextGeneration[i]) == 10) {
-						maxFitnessAchieved = true;
-						fitnessCrossover = true;
-						break;
+		String crossover = "cross-over";
+		String mutation = "mutation";
+		int counter = 0;
+		int total = 0;
+		int[] popCount = new int[20];
+		int[] pcoCount = new int[4];
+		/* 
+		 * Experiment 1
+		 * Run genetic algorithm 20 times at pco = 0.7
+		 * Print average number of generations to reach max
+		 * fitness value.
+		 *  
+		 */
+	  
+		Population[] popEx = new Population[20];
+		for(int i = 0 ; i < popEx.length ; i++)
+		{
+			popEx[i] = new Population(0.7);
+			popEx[i].printPopulation();
+			while (!popEx[i].maxFitnessAchieved()) {
+				popEx[i].crossover();
+				if (!popEx[i].maxFitnessAchieved()) {
+					popEx[i].mutate();
+					if (popEx[i].maxFitnessAchieved()){
+						popEx[i].setMaxFitMethod(mutation);
 					}
-				}
-				
-				if (!maxFitnessAchieved) {
-							
-					mutatedGeneration = mutatorGenerator(nextGeneration);
-//					System.out.println("Mutated generation: ");
-					for(int i = 0; i < mutatedGeneration.length; i++) {
-						System.out.println( String.format("%10s\t", 
-											Integer.toBinaryString(mutatedGeneration[i])).replace(' ', '0') 
-											+ calculateFitness(mutatedGeneration[i]));
-						// Check fitness value
-						if (calculateFitness(mutatedGeneration[i]) == 10) {
-							maxFitnessAchieved = true;
-							fitnessMutation = true;
-							break;
-						}
-					}
-				}
-				
-				if (!maxFitnessAchieved){
-					population = mutatedGeneration;
 				}
 				else {
-					break;			
+					popEx[i].setMaxFitMethod(crossover);
 				}
 				
 			}
-			
-			System.out.println("Max fitness achieved at generation " + generationCount);
-			if (fitnessMutation) {
-				System.out.println("By Mutation");
-				mutStr = "Mutation";
-				sPco[ii] = mutStr;
-			}
-			else if (fitnessCrossover){
-				System.out.println("By Cross-Over");	
-				crossStr = "Cross-Over";
-				sPco[ii] = crossStr;
-			}	
-			System.out.println("\n\n\n\n\n\n");
-			nPco[ii] = generationCount;
+			System.out.println(popEx[i].getGenCount());
+			counter++;
+			total += popEx[i].getGenCount();
+			System.out.println("Average Count = " + total/counter);
+			System.out.println(popEx[i].getMaxFitMethod()+"\n\n\n");
+			popCount[i] = popEx[i].getGenCount();
 		}
-		for(int i = 0 ; i < 5 ; i++)
+		
+		PopulationPlot popChart = new PopulationPlot(
+				"Generations vs Chromosomes", 
+				"Number of Generations vs Chromosomes", popCount);
+		
+		popChart.pack();
+	    popChart.setVisible(true);
+	   
+		/* 
+		 * Experiment 2
+		 * Perform experiment 1 with same initial population for each run:
+		 * i) pco = 0.3
+		 * ii) pco = 0.5
+		 * iii) pco = 0.9
+		 * iv) pco = 0
+		 * 
+		 */
+		counter = 0;
+		total = 0;
+		int j = 0;
+		int pcoCounters = 0;
+		int[] avgPco = new int[4];
+		int seedVal = (int) System.currentTimeMillis();
+		Population[] populations = new Population[20];
+		double[] pco = {0.3, 0.5, 0.9, 0};
+		for(int pcoLoop = 1 ; pcoLoop < 21 ; pcoLoop++)
 		{
-			System.out.println("mPco" + i + ": " + nPco[i]);
-			System.out.println("sPco: " + sPco[i]);
+			for(int i = 0 ; i < pco.length ; i++)
+			{
+				populations[i] = new Population(seedVal);
+				populations[i].setPco(pco[i]);
+				System.out.println("====PCO " + populations[i].getPco() + "====");
+				
+				populations[i].printPopulation();
+			
+				while (!populations[i].maxFitnessAchieved()) {
+					populations[i].crossover();
+					if (!populations[i].maxFitnessAchieved()) {
+						populations[i].mutate();
+						if (populations[i].maxFitnessAchieved()){
+							populations[i].setMaxFitMethod(mutation);
+						}
+					}
+					else {
+						populations[i].setMaxFitMethod(crossover);
+					}
+					
+				}
+				System.out.println(populations[i].getGenCount());
+				counter++;
+				total += popEx[i].getGenCount();
+				System.out.println("Average Count = " + total/counter);
+				System.out.println(populations[i].getMaxFitMethod()+"\n\n\n");
+				pcoCount[i] = popEx[i].getGenCount();
+				pcoCounters += pcoCount[i];
+			}
+			System.out.println("j:" + j + " pcoLoop:" + pcoLoop);
+			if(pcoLoop%5 == 0)
+			{
+				avgPco[j] = pcoCounters/20;
+				++j;
+			}
 		}
+		for(int a = 0 ; a < 4 ; a++)
+		{
+			System.out.println(avgPco[a]);
+		}
+		PopulationPlot pcoChart = new PopulationPlot(
+				"Generations vs PCO", 
+				"Number of Generations vs PCO", pco, pcoCount);
+		
+		pcoChart.pack();
+	    RefineryUtilities.centerFrameOnScreen(pcoChart);
+	    pcoChart.setVisible(true);
 	}
+
+	
+	
 }
---
+++ src/hw1/PopulationPlot.java
@@ -0,0 +1,56 @@
+package hw1;
+
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+@SuppressWarnings("serial")
+public class PopulationPlot extends ApplicationFrame
+{
+	public PopulationPlot(String applicationTitle, String chartTitle, int[] popCount)
+	{
+		super(applicationTitle);
+		JFreeChart lineChart = ChartFactory.createBarChart3D(
+			chartTitle,"Chromosomes", "Number of Generations", createDataset(popCount), PlotOrientation.VERTICAL, true,true,false);
+         
+		ChartPanel chartPanel = new ChartPanel(lineChart);
+		chartPanel.setPreferredSize( new java.awt.Dimension(500 , 300));
+		setContentPane(chartPanel);
+	}
+
+	public PopulationPlot(String applicationTitle, String chartTitle, double[] popPco, int[] popCount)
+	{
+		super(applicationTitle);
+		JFreeChart lineChart = ChartFactory.createBarChart3D(
+			chartTitle,"PCO", "Number of Generations", createDataset2(popPco, popCount), PlotOrientation.VERTICAL, true,true,false);
+         
+		ChartPanel chartPanel = new ChartPanel(lineChart);
+		chartPanel.setPreferredSize( new java.awt.Dimension(500 , 300));
+		setContentPane(chartPanel);
+	}
+	
+	public DefaultCategoryDataset createDataset(int[] popCount)
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		int j = 0;
+		for(int i = 0 ; i < popCount.length ; i++)
+		{
+			j++;
+			dataset.addValue(popCount[i], "Chromosome", Integer.toString(j));
+		}
+		return dataset;
+	}
+	
+	public DefaultCategoryDataset createDataset2(double[] popPco, int[] popCount)
+	{
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		for(int i = 0 ; i < popPco.length ; i++)
+		{
+			dataset.addValue(popCount[i], "Chromosome", String.valueOf(popPco[i]));
+		}
+		return dataset;
+	}
+}
